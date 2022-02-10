852c4deda10679cdee3c63a12fc8516ed6f7a975
Andy Chu <andyzhuaz@outlook.com>
2022-02-10 13:14:11 +0800

    add: desp in params,result exception handling, send_text can accept list as pushkey (#1)
    * add: desp in params
    
    * add: result exception handling, send_text can accept list as pushkey, temporarily add unit tests for test_send
    
    * fix: format
    
    * add: Remove redundant code, add unit tests, add doc, renew send function
    
    * fix: unittest with mock, response load with requests.get().json(), extract get request method, remove PushDeer.type PushDeer.result
    
    * fix: requirements.txt
    
    * fix: requirements.txt
    
    * fix: parameter type, remove unnecessary exception catches

--- src/pypushdeer/pypushdeer.py
+++ src/pypushdeer/pypushdeer.py
@@ -1,13 +1,11 @@
-from typing import Optional
-
+import json
+from typing import Optional, Union
 import requests
 
 
 class PushDeer:
     server = "https://api2.pushdeer.com"
-
     endpoint = "/message/push"
-
     pushkey = None
 
     def __init__(self, server: Optional[str] = None, pushkey: Optional[str] = None):
@@ -16,33 +14,86 @@ class PushDeer:
         if pushkey:
             self.pushkey = pushkey
 
-    def send_text(self, text: str, server: Optional[str] = None, pushkey: Optional[str] = None):
+    def push(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
+             pushkey: Union[str, list, None] = None, text_type: Optional[str] = None):
+        """
+        Basic method of calling /message/push API
+        @param text_type:
+        @param text:
+        @param desp:
+        @param server:
+        @param pushkey:
+        @return:
+        """
         if not pushkey and not self.pushkey:
             raise ValueError("pushkey must be specified")
+        if type(pushkey) is list:
+            count = 0
+            for key in pushkey:
+                r_json = self.send_push_request(desp, key, server, text, text_type)
+                if r_json["content"]["result"]:
+                    result = json.loads(r_json["content"]["result"][0])
+                    if result["success"] == 'ok':
+                        count += 1
+                    else:
+                        pass
+                else:
+                    return False
+            if count == len(pushkey):
+                return True
+            else:
+                return False
+        else:
+            r_json = self.send_push_request(desp, pushkey, server, text, text_type)
+            if r_json["content"]["result"]:
+                result = json.loads(r_json["content"]["result"][0])
+                if result["success"] == "ok":
+                    return True
+                else:
+                    return False
+            else:
+                return False
 
-        requests.get(server or self.server + self.endpoint, params={
-            "pushkey": self.pushkey or pushkey,
+    def send_push_request(self, desp, key, server, text, text_type):
+        return requests.get(server or self.server + self.endpoint, params={
+            "pushkey": self.pushkey or key,
             "text": text,
-            "type": "text"
-        })
+            "type": text_type,
+            "desp": desp,
+        }).json()
 
-    def send_markdown(self, text: str, server: Optional[str] = None, pushkey: Optional[str] = None):
-        if not pushkey and not self.pushkey:
-            raise ValueError("pushkey must be specified")
+    def send_text(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
+                  pushkey: Union[str, list, None] = None):
+        """
+        Any text are accepted when type is text.
+        @param text: message : text
+        @param desp: the second part of the message
+        @param server: optional server http address
+        @param pushkey: PushDeer pushkeys, multiple pushkey use list of string, single pushkey use string
+        @return: Boolean
+        """
+        return self.push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='text')
 
-        requests.get(server or self.server + self.endpoint, params={
-            "pushkey": self.pushkey or pushkey,
-            "text": text,
-            "type": "markdown"
-        })
+    def send_markdown(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
+                      pushkey: Union[str, list, None] = None):
+        """
+        Text in Markdown format are accepted when type is markdown.
+        @param text: message : text in Markdown
+        @param desp: the second part of the message
+        @param server: optional server http address
+        @param pushkey: PushDeer pushkeys, multiple pushkey use list of string, single pushkey use string
+        @return: Boolean
+        """
+        return self.push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='markdown')
 
-    def send_image(self, image_url_or_base64: str, server: Optional[str] = None, pushkey: Optional[str] = None):
-
-        if not pushkey and not self.pushkey:
-            raise ValueError("pushkey must be specified")
-
-        requests.get(server or self.server + self.endpoint, params={
-            "pushkey": self.pushkey or pushkey,
-            "text": image_url_or_base64,
-            "type": "image"
-        })
+    def send_image(self, image_url: str, desp: Optional[str] = None, server: Optional[str] = None,
+                   pushkey: Union[str, list, None] = None):
+        """
+        Only image url are accepted by API now, when type is image.
+        @param image_url: message : image URL
+        @param desp: the second part of the message
+        @param server: optional server http address
+        @param pushkey: PushDeer pushkeys, multiple pushkey use list of string, single pushkey use string
+        @return: Boolean
+        """
+        return self.push(text=image_url, desp=desp, server=server, pushkey=pushkey, text_type='image')
