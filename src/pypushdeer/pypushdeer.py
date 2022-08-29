ad5b7a7d12173c57f63e08b787c353f086861ffa
xin <luoxin9712@163.com>
2022-08-29 21:40:47 +0800

    feat: add args when push

--- src/pypushdeer/pypushdeer.py
+++ src/pypushdeer/pypushdeer.py
@@ -15,12 +15,12 @@ class PushDeer:
             self.pushkey = pushkey
 
     def _push(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
-              pushkey: Optional[str] = None, text_type: Optional[str] = None):
+              pushkey: Optional[str] = None, text_type: Optional[str] = None, **kwargs):
 
         if not pushkey and not self.pushkey:
             raise ValueError("pushkey must be specified")
 
-        res = self._send_push_request(desp, pushkey or self.pushkey, server or self.server, text, text_type)
+        res = self._send_push_request(desp, pushkey or self.pushkey, server or self.server, text, text_type, **kwargs)
         if res["content"]["result"]:
             result = json.loads(res["content"]["result"][0])
             if result["success"] == "ok":
@@ -30,16 +30,16 @@ class PushDeer:
         else:
             return False
 
-    def _send_push_request(self, desp, key, server, text, type):
+    def _send_push_request(self, desp, key, server, text, type, **kwargs):
         return requests.get(server + self.endpoint, params={
             "pushkey": key,
             "text": text,
             "type": type,
             "desp": desp,
-        }).json()
+        }, **kwargs).json()
 
     def send_text(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
-                  pushkey: Union[str, list, None] = None):
+                  pushkey: Union[str, list, None] = None, **kwargs):
         """
         Any text are accepted when type is text.
         @param text: message : text
@@ -48,10 +48,10 @@ class PushDeer:
         @param pushkey: pushDeer pushkey
         @return: success or not
         """
-        return self._push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='text')
+        return self._push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='text', **kwargs)
 
     def send_markdown(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
-                      pushkey: Union[str, list, None] = None):
+                      pushkey: Union[str, list, None] = None, **kwargs):
         """
         Text in Markdown format are accepted when type is markdown.
         @param text: message : text in markdown
@@ -60,10 +60,10 @@ class PushDeer:
         @param pushkey: pushDeer pushkey
         @return: success or not
         """
-        return self._push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='markdown')
+        return self._push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='markdown', **kwargs)
 
     def send_image(self, image_src: str, desp: Optional[str] = None, server: Optional[str] = None,
-                   pushkey: Union[str, list, None] = None):
+                   pushkey: Union[str, list, None] = None, **kwargs):
         """
         Only image src are accepted by API now, when type is image.
         @param image_src: message : image URL
@@ -72,7 +72,7 @@ class PushDeer:
         @param pushkey: pushDeer pushkey
         @return: success or not
         """
-        return self._push(text=image_src, desp=desp, server=server, pushkey=pushkey, text_type='image')
+        return self._push(text=image_src, desp=desp, server=server, pushkey=pushkey, text_type='image', **kwargs)
 
 
 if __name__ == "__main__":
